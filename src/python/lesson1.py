0af19e7f64418c4e1463a6643b7bb5465c59521f
siavash rahimi <rahimi.f.h@gmail.com>
2024-12-09 20:34:29 +0330

    start python

---
+++ src/python/lesson1.py
@@ -0,0 +1,97 @@
+# *************************** string ************************************
+name =  "ALi"
+lastname = 'salehi'
+sen = "This is a test for python strings"
+
+# print(sen[:14])
+# print(len(sen))
+fullNaame = f"{name} {lastname}"
+# print(fullNaame)
+
+code = 48346468
+# *********************************** number *******************************
+# print(type(code))
+code = str(code)
+# print(type(code))
+
+idcode = "684648"
+
+number = -546486
+
+# print(type(name))
+# idcode = int(name)
+# print(type(name))
+
+# ********************************** float *******************************
+
+score = 3.89
+# print(type(score))
+
+# print(float(number))
+# print(float(5))
+
+# *********************************** boolean ****************************
+
+admin = True
+turnOff = False
+
+print(int(turnOff))
+print(bool(name))
+
+# >>> 5 * 5
+# 25
+# >>> 4 / 3
+# 1.3333333333333333
+# >>> 8 + 6
+# 14
+# >>> 7 - 9
+# -2
+# >>> 12 % 2
+# 0
+# >>> 13 // 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
