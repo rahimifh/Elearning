a953bd69cd9bab332b8fb47e365d1758bcecc17d
siavash rahimi <rahimi.f.h@gmail.com>
2024-12-23 16:29:23 +0330

    lesson3

---
+++ src/python/lesson3.py
@@ -0,0 +1,12 @@
+
+while True:
+    age = int(input("How old are you: "))
+    if age < 10 and age > 0: 
+        print("you are child!") 
+
+    elif age >= 10 and age < 30:
+        print("You are young!")
+    elif age >= 30:
+        print("you are old")
+    else:
+        print("invalid age")
