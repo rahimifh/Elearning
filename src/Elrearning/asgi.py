13b48d0576286ea1261bcbd3eccb3eb3454d1b3a
Siavash Rahimi <rahimi.f.h@gmail.com>
2025-01-11 20:51:50 +0330

    start django project anf delete extra file

---
+++ src/Elrearning/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Elrearning project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Elrearning.settings')
+
+application = get_asgi_application()
