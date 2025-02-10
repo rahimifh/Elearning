c38ccd6a15d1f7d1b0848b8d511bb24f83e0ef5f
Siavash Rahimi <rahimi.f.h@gmail.com>
2025-02-10 20:11:45 +0330

    start blog

--- src/Elrearning/urls.py
+++ src/Elrearning/urls.py
@@ -7,7 +7,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("courses.urls"), name="courses"),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
