e2079aa9652fc11bf6a8441291dd86b0f4f7d58d
Siavash Rahimi <rahimi.f.h@gmail.com>
2025-03-05 19:34:50 +0330

    add tags to posts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path(
+        'tag/<slug:tag_slug>/', views.post_list, name='post_by_tag'
+        ),
+    path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('com/', views.components, name='components'),
     path(
